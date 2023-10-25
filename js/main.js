26be23587ee930f441c0bbd7cdd469d05504b7d8
Avakov-Mike <michaelsh1p@mail.ru>
2023-10-25 11:29:03 +0300

    fixed section graphics

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.graphics-content_bottom-sliders', {
+  // pagination: {
+  //   el: '.swiper-pagination',
+  //   clickable: true,
+  // },
+  navigation: {
+    nextEl: '.graphics-content_top-right_button-right',
+    prevEl: '.graphics-content_top-right_button-left',
+  },
+});
