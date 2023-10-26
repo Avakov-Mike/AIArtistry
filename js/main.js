04a67bf6a68a93290e7c032a467623d1450044df
Avakov-Mike <michaelsh1p@mail.ru>
2023-10-26 11:44:34 +0300

    create section testimonials

--- js/main.js
+++ js/main.js
@@ -8,3 +8,14 @@ const swiper = new Swiper('.graphics-content_bottom-sliders', {
     prevEl: '.graphics-content_top-right_button-left',
   },
 });
+
+const newSwiper = new Swiper('.testimonials-content_sliders', {
+  // pagination: {
+  //   el: '.swiper-pagination',
+  //   clickable: true,
+  // },
+  navigation: {
+    nextEl: '.testimonials-content_slide-right_button-right',
+    prevEl: '.testimonials-content_slide-right_button-left',
+  },
+});
